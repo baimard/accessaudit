4a2635a8a7f2d9591f888406a48b3bda9ae301ec
Benjamin <benjamin@cybercorp.fr>
2026-07-29 15:37:11 +0200

    feat: secure the audit page for administrators

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -2,28 +2,18 @@
 
 declare(strict_types=1);
 
-namespace OCA\AppTemplate\Controller;
+namespace OCA\AccessAudit\Controller;
 
-use OCA\AppTemplate\AppInfo\Application;
+use OCA\AccessAudit\AppInfo\Application;
 use OCP\AppFramework\Controller;
-use OCP\AppFramework\Http\Attribute\FrontpageRoute;
-use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+use OCP\AppFramework\Http\Attribute\AdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
-use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\TemplateResponse;
 
-/**
- * @psalm-suppress UnusedClass
- */
 class PageController extends Controller {
+	#[AdminRequired]
 	#[NoCSRFRequired]
-	#[NoAdminRequired]
-	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
-	#[FrontpageRoute(verb: 'GET', url: '/')]
 	public function index(): TemplateResponse {
-		return new TemplateResponse(
-			Application::APP_ID,
-			'index',
-		);
+		return new TemplateResponse(Application::APP_ID, 'index');
 	}
 }
