3bcd8cf0cf53f6baded22a7668dd2a032d08cb2c
Benjamin <benjamin@cybercorp.fr>
2026-07-29 16:02:43 +0200

    Allow group-enabled users to access Access Audit

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -6,12 +6,12 @@ namespace OCA\AccessAudit\Controller;
 
 use OCA\AccessAudit\AppInfo\Application;
 use OCP\AppFramework\Controller;
-use OCP\AppFramework\Http\Attribute\AdminRequired;
+use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\TemplateResponse;
 
 class PageController extends Controller {
-	#[AdminRequired]
+	#[NoAdminRequired]
 	#[NoCSRFRequired]
 	public function index(): TemplateResponse {
 		return new TemplateResponse(Application::APP_ID, 'index');
